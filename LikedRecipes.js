2b692e6b9f9725a05c12daff7ec3358a575575e2
Afroze Najam <150817540+afrozepal@users.noreply.github.com>
2024-05-23 18:49:24 +0500

    Add files via upload

---
+++ LikedRecipes.js
@@ -0,0 +1,15 @@
+import {PNav, PNav2} from '../components/Premiumnav'
+import PIbar from '../components/Pinputbar'
+
+function LikedRecipes() {
+  return (
+    <div className="pmainc">
+    <PNav></PNav>
+    <PNav2></PNav2>
+    <PIbar></PIbar>
+    </div>
+      
+  );
+}
+
+export default LikedRecipes;
